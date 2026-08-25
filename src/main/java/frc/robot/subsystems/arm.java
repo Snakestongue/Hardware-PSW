464a9607388fc012bc2c0cbddbffd2a87e2d61f5
Snakestongue <maeher.grover@gmail.com>
2026-08-24 22:19:07 -0400

    Commit 1

---
+++ src/main/java/frc/robot/subsystems/arm.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class arm extends SubsystemBase{
+   //Instructions: Add 2 objects for a motor and encoder and print encoder position in SmartDashboard.
+    @Override
+    public void periodic() {
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/laser.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class laser extends SubsystemBase{
+    //Create 2 objects for a motor and a LaserCAN. Let the motor run at 50% until laserbeam breaks at less then 50mm
+}
